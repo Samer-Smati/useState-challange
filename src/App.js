d7eafbf89c54e3ca5cb228820c95fdbc86da1b0b
Samer-Smati <samersmati@gmail.om>
2022-03-25 22:06:55 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
-
+import {Table} from 'react-bootstrap'
+import CustomModal from './components/ui/modals'
 function App() {
+  const [data,setData] = useState([])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomModal setdata={setData}/>
+      {data.length ? 
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Birthday</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((el,key)=>
+            <tr key={key}>
+              <td>{key+1}</td>
+            <td>{el.firstName}</td>
+            <td>{el.lastName}</td>
+            <td>{el.email}</td>
+            <td>{el.date}</td>
+          </tr>
+            )}
+          </tbody>
+        </Table>
+      
+      : 
+      <div>Please create data</div>
+      }
+      
     </div>
   );
 }
